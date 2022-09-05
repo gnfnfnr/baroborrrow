b0ec600410e1bb0e10024cf839ca24f15eec0748
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-09-05 15:17:12 +0900

    Add back file and set front file
    백엔드 파일 추가 그리고 front 초기 설정( 글씨, 파일 정리)

---
+++ baro_front/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalStyle } from "./style";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <div>ㅇㄹㅇㄹ</div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
